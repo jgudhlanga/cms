2a3b641f89d5706a39614ec40fe3cbe28bfc4330
James Gudhlanga <jimmyneds@gmail.com>
2025-04-30 19:29:43 +0200

    department view metadata

--- resources/js/pages/institution/departments/Show.vue
+++ resources/js/pages/institution/departments/Show.vue
@@ -1,9 +1,20 @@
 <script setup lang="ts">
 import PageContainer from '@/components/core/page/PageContainer.vue';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import AboutUs from '@/pages/institution/departments/partials/view/AboutUs.vue';
+import Announcements from '@/pages/institution/departments/partials/view/Announcements.vue';
+import Calendar from '@/pages/institution/departments/partials/view/Calendar.vue';
+import Classes from '@/pages/institution/departments/partials/view/Classes.vue';
+import Courses from '@/pages/institution/departments/partials/view/Courses.vue';
+import Levels from '@/pages/institution/departments/partials/view/Levels.vue';
+import ProvisionalClasses from '@/pages/institution/departments/partials/view/ProvisionalClasses.vue';
+import Staff from '@/pages/institution/departments/partials/view/Staff.vue';
 import { AuthObject } from '@/types/data-pagination';
 import { InstitutionDepartment } from '@/types/institution';
 import type { Link } from '@/types/ui';
+import { CustomTab } from '@/types/utils';
 import { Head } from '@inertiajs/vue3';
+import { ref } from 'vue';
 
 interface Props {
     department: InstitutionDepartment;
@@ -18,11 +29,32 @@ const breadcrumbs: Array<Link> = [
     { transChoiceKey: 'department', href: route('institution-departments.index') },
     { title: department.attributes.department },
 ];
+
+const defaultValue = ref('about_us');
+const tabs: Array<CustomTab> = [
+    { label: 'About Us', value: 'about_us', component: AboutUs },
+    { label: 'Courses', value: 'courses', component: Courses },
+    { label: 'Levels', value: 'levels', component: Levels },
+    { label: 'Staff', value: 'staff', component: Staff },
+    { label: 'Provisional Classes', value: 'provisional_classes', component: ProvisionalClasses },
+    { label: 'Classes', value: 'classes', component: Classes },
+    { label: 'Calendar', value: 'calendar', component: Calendar },
+    { label: 'Announcements', value: 'announcements', component: Announcements },
+];
 </script>
 
 <template>
     <Head :title="$tChoice('trans.department', 2)" />
     <PageContainer :breadcrumbs="breadcrumbs">
-
+        <Tabs :default-value="defaultValue">
+            <TabsList class="w-full">
+                <TabsTrigger v-for="tab in tabs" :key="'tab_' + tab.value" :value="tab.value" class="text-xs font-light uppercase">
+                    {{ tab.label }}
+                </TabsTrigger>
+            </TabsList>
+            <TabsContent v-for="tab in tabs" :value="tab.value" :key="'content_' + tab.value">
+                <component :is="tab.component" />
+            </TabsContent>
+        </Tabs>
     </PageContainer>
 </template>
--- resources/js/types/utils.ts
+++ resources/js/types/utils.ts
@@ -1,88 +1,92 @@
-import { type VNodeChild } from 'vue';
 import type { ColorVariant } from '@/enums/colors';
 import type { IconName } from '@/enums/icons';
+import { Component, type VNodeChild } from 'vue';
 
 export type Link = {
-	transName?: string;
-	url?: string;
-	title?: string;
-	name?: string;
-	params?: any,
-	icon?: string;
-	group?: string;
-	children?: Array<Link>
-}
+    transName?: string;
+    url?: string;
+    title?: string;
+    name?: string;
+    params?: any;
+    icon?: string;
+    group?: string;
+    children?: Array<Link>;
+};
 export type Dropdown = {
-	id: string;
-	title: string;
-	name?: string;
-	description?: string;
-}
+    id: string;
+    title: string;
+    name?: string;
+    description?: string;
+};
 
 export type SelectOption = {
-	auxLabel?: string;
-	label: string;
-	relationshipOneLabel?: string;
-	relationshipOneValue?: string;
-	value: string | number;
-}
+    auxLabel?: string;
+    label: string;
+    relationshipOneLabel?: string;
+    relationshipOneValue?: string;
+    value: string | number;
+};
 
 export type DropdownButtonOption = {
-	label: string,
-	key: string,
-	icon: () => VNodeChild,
-}
+    label: string;
+    key: string;
+    icon: () => VNodeChild;
+};
 
 export type ActionOption = {
-	name: string,
-	title: string,
-}
+    name: string;
+    title: string;
+};
 
 export type ItemOptionSelectParams = {
-	option: DropdownButtonOption,
-	editAction: () => void,
-	deleteAction: () => void,
-	goBackAction: () => void,
-	viewAction: () => void,
-	importAction: () => void,
-	createAction: () => void,
-	archiveAction: () => void,
-	restoreAction: () => void,
-
-}
+    option: DropdownButtonOption;
+    editAction: () => void;
+    deleteAction: () => void;
+    goBackAction: () => void;
+    viewAction: () => void;
+    importAction: () => void;
+    createAction: () => void;
+    archiveAction: () => void;
+    restoreAction: () => void;
+};
 
 export type IconButtonParams = {
-	action: () => void,
-	disable: boolean | number | null,
-	icon: IconName
-	type?: ColorVariant
-	iconColor?: ColorVariant,
-	iconSize?: string,
-}
+    action: () => void;
+    disable: boolean | number | null;
+    icon: IconName;
+    type?: ColorVariant;
+    iconColor?: ColorVariant;
+    iconSize?: string;
+};
 
 export type NotificationParam = {
-	title: string,
-	description: string,
-}
+    title: string;
+    description: string;
+};
 export type TableActionButton = {
-	isArchived?: boolean,
-	hasView?: boolean,
-	onEdit?: () => void | null,
-	onView?: () => void | null,
-	onArchive?: () => void | null,
-	onRestore?: () => void | null,
-	onDelete?: () => void | null,
-}
+    isArchived?: boolean;
+    hasView?: boolean;
+    onEdit?: () => void | null;
+    onView?: () => void | null;
+    onArchive?: () => void | null;
+    onRestore?: () => void | null;
+    onDelete?: () => void | null;
+};
 
 export type TableButton = {
-	title: string,
-	variant?: ColorVariant,
-	classes?: string,
-	onClick?: () => void | null
-}
+    title: string;
+    variant?: ColorVariant;
+    classes?: string;
+    onClick?: () => void | null;
+};
 export type ValueAndLabel = {
-	value: any,
-	transKey?: string,
-	transChoiceKey?: string,
-	isBoolean?: boolean
-}
+    value: any;
+    transKey?: string;
+    transChoiceKey?: string;
+    isBoolean?: boolean;
+};
+export type CustomTab = {
+    label: string;
+    value: string;
+    component: Component;
+};
