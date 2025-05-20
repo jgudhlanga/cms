95b7c3f7770755d6ae33364361e26f0563d9643f
penstej <penstejdevelopers@gmail.com>
2025-05-20 06:48:49 +0200

    package update

--- resources/js/components/core/alert/BaseAlert.vue
+++ resources/js/components/core/alert/BaseAlert.vue
@@ -32,9 +32,9 @@ const computedClass = computed(() =>
 
 <template>
 	<Alert :class="computedClass">
-		<component :is="icons[iconVariants[type] as IconName]" class="size-6" />
-		<AlertTitle v-if="title" class="uppercase font-bold mx-3">{{ title }}</AlertTitle>
-		<AlertDescription class="m-3">
+		<component :is="icons[iconVariants[type] as IconName]" class="size-9" />
+		<AlertTitle v-if="title" class="uppercase font-bold mx-4 mt-2">{{ title }}</AlertTitle>
+		<AlertDescription class="m-4">
 			{{ description }}
 		</AlertDescription>
 	</Alert>
