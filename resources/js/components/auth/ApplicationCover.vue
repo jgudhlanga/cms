d6a96d41141ee1d2ffad402d35bec6bc21f985be
James Gudhlanga <jimmyneds@gmail.com>
2025-06-27 20:44:52 +0200

    application back ground fix

--- resources/js/components/auth/ApplicationCover.vue
+++ resources/js/components/auth/ApplicationCover.vue
@@ -6,7 +6,7 @@ import InfiniteBackground from '@/components/auth/InfiniteBackground.vue';
 
 <template>
     <InfiniteBackground />
-    <div class="flex flex-col items-center justify-center space-y-4 overflow-scroll py-8">
+    <div class="flex flex-col items-center justify-center space-y-4 overflow-hidden py-8">
         <div class="my-5 flex size-20 flex-col items-center justify-center">
             <AppLogo classes="border-2 border-white" />
         </div>
