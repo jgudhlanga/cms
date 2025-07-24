45f7b63081aa3574986d6f8961f2d5180a414434
James Gudhlanga <jimmyneds@gmail.com>
2025-07-24 17:41:22 +0200

    package upgrade

--- resources/js/pages/portal/student/ApplicationTrack.vue
+++ resources/js/pages/portal/student/ApplicationTrack.vue
@@ -8,6 +8,8 @@ import { Student, StudentProgram } from '@/types/students';
 import { BreadcrumbItemInterface } from '@/types/ui';
 import { Head } from '@inertiajs/vue3';
 import { computed, onMounted, ref } from 'vue';
+import { TimelineStep } from '@/types/utils';
+import { trans_choice } from 'laravel-vue-i18n';
 
 interface Props {
     auth: AuthObject;
@@ -40,6 +42,21 @@ onMounted(async () => {
 // Example status-to-step mapping
 const steps = ['Submitted', 'Under Review', 'Interview', 'Final Decision', 'Completed'];
 
+const mySteps = computed(() => {
+    return workflowSteps.value?.map(
+        (step: DepartmentApplicationStep, index: number) =>
+            <TimelineStep>{
+                title: step.attributes?.workflowStep,
+                description: step.attributes?.workflowStepDescription,
+                timelineMarker: step.attributes?.position?.toString() ?? '',
+                label: `${trans_choice('trans.step', 1)} ${index + 1}`,
+                props: {
+                    step,
+                    action: () => {},
+                },
+            },
+    );
+});
 // Current step index based on status
 const currentStepIndex = computed(() => {
     const index = steps.findIndex((step) => step.toLowerCase() === applications.status.toLowerCase());
@@ -61,6 +78,7 @@ const progressPercent = computed(() => {
         </template>
         <template v-else>
             <div>
+                {{ mySteps }}
                 <div class="h-6 w-full overflow-hidden rounded-full bg-gray-200">
                     <div class="bg-primary h-full transition-all duration-300" :style="{ width: progressPercent + '%' }"></div>
                 </div>
