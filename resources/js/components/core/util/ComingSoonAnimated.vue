4fbfd0a33d7d5c2497970e034ccdb094e6fab9f8
penstej <penstejdevelopers@gmail.com>
2025-09-22 13:22:14 +0200

    added button to go back

--- resources/js/components/core/util/ComingSoonAnimated.vue
+++ resources/js/components/core/util/ComingSoonAnimated.vue
@@ -1,6 +1,10 @@
 <script lang="ts" setup>
-import { ref, onMounted, onUnmounted } from 'vue';
+import { BaseButton } from '@/components/core/button';
+import BaseIcon from '@/components/core/icon/BaseIcon.vue';
+import { useUtils } from '@/composables/core/useUtils';
+import { IconName } from '@/lib/icons';
 import lottie from 'lottie-web';
+import { onMounted, onUnmounted, ref } from 'vue';
 
 const targetDate = new Date('2025-09-31T23:59:59');
 
@@ -44,17 +48,18 @@ const showCountdown = false;
 onUnmounted(() => {
     clearInterval(timerInterval);
 });
+const { goBack } = useUtils();
 </script>
 
 <template>
-    <div class="flex h-[70vh] flex-col items-center justify-center bg-transparent p-6 " style="height: 70vh">
+    <div class="flex h-[70vh] flex-col items-center justify-center bg-transparent p-6" style="height: 70vh">
         <!-- Lottie animation -->
-        <div ref="lottieContainer" class="w-40 h-40 sm:w-52 sm:h-52 md:w-64 md:h-64 mb-6"></div>
+        <div ref="lottieContainer" class="mb-6 h-40 w-40 sm:h-52 sm:w-52 md:h-64 md:w-64"></div>
 
         <!-- Text content -->
-        <div class="flex flex-col w-full space-y-4 text-center">
-            <h1 class="text-5xl font-bold uppercase text-destructive">Coming Soon</h1>
-            <div class="flex items-center justify-center w-full text-lg sm:text-lg">We're still configuring the system. Stay tuned!</div>
+        <div class="flex w-full flex-col space-y-4 text-center">
+            <h1 class="text-destructive text-5xl font-bold uppercase">Coming Soon</h1>
+            <div class="flex w-full items-center justify-center text-lg sm:text-lg">We're still configuring the system. Stay tuned!</div>
 
             <!-- Countdown Timer -->
             <div v-if="showCountdown" class="mt-4 flex justify-center space-x-6 font-mono text-2xl">
@@ -74,6 +79,12 @@ onUnmounted(() => {
                     <span class="block text-5xl font-bold">{{ seconds }}</span>
                     <span class="text-sm uppercase">Seconds</span>
                 </div>
+            </div>
+            <div class="mt-4 flex justify-center space-x-6 font-mono text-2xl">
+                <BaseButton @click="goBack()">
+                    <BaseIcon :name="IconName.chevron_double_left" class="mr-2 size-5" />
+                    <span>Go Back</span>
+                </BaseButton>
             </div>
         </div>
     </div>
