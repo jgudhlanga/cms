3404dbfc2b8c119d3cd2166274587a76fdcba62b
penstej <penstejdevelopers@gmail.com>
2025-09-25 18:12:29 +0200

    fixes on ui

--- resources/js/components/core/form/text/BaseInput.vue
+++ resources/js/components/core/form/text/BaseInput.vue
@@ -1,56 +1,66 @@
 <script setup lang="ts">
-import { onMounted } from 'vue';
+import InputError from '@/components/core/form/InputError.vue';
+import RequiredIndicator from '@/components/core/form/RequiredIndicator.vue';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
-import InputError from '@/components/core/form/InputError.vue';
 import { TextFieldType } from '@/enums/inputs';
 import { cn } from '@/lib/utils';
-import RequiredIndicator from '@/components/core/form/RequiredIndicator.vue';
-interface Props {
-    inputId: string,
-    label?: string,
-    type?: TextFieldType,
-    classes?: string,
-    error?: string | object,
-    inputAutoFocus?: boolean,
-    labelUppercase?: boolean,
-    verticalLayout?: boolean,
-    isRequired?: boolean,
+import { onMounted } from 'vue';
 
+interface Props {
+    inputId: string;
+    label?: string;
+    type?: TextFieldType;
+    classes?: string;
+    error?: string | object;
+    inputAutoFocus?: boolean;
+    labelUppercase?: boolean;
+    verticalLayout?: boolean;
+    isRequired?: boolean;
 }
+
 const props = withDefaults(defineProps<Props>(), {
-	type: TextFieldType.text,
-	labelUppercase:false,
-    verticalLayout:true,
+    type: TextFieldType.text,
+    labelUppercase: false,
+    verticalLayout: true,
     isRequired: false,
 });
 
-const baseClasses = 'px-3 py-4 focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-0';
+const baseClasses = `
+  px-3 py-4
+  border border-input
+  bg-background text-foreground
+  placeholder:text-muted-foreground
+  rounded-md
+  focus-visible:outline-none
+  focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-0
+  disabled:cursor-not-allowed disabled:opacity-50
+`;
 
 const setAutoFocus = () => {
-	const inputElement = document.getElementById(props.inputId) as HTMLInputElement;
-	if (inputElement) {
-		props.inputAutoFocus && inputElement.focus();
-	}
+    const inputElement = document.getElementById(props.inputId) as HTMLInputElement;
+    if (inputElement && props.inputAutoFocus) {
+        inputElement.focus();
+    }
 };
 
 onMounted(() => setAutoFocus());
-
 </script>
 
 <template>
-	<div class="flex flex-col">
-		<div :class="cn('flex space-x-3', verticalLayout && 'flex-col space-y-2')">
-			<Label :class="cn(error && 'text-destructive', labelUppercase && 'uppercase', !verticalLayout && 'flex items-center w-1/4')" v-if="label" :for="inputId">
-                {{ label }}<RequiredIndicator v-if="isRequired"/>
+    <div class="flex flex-col">
+        <div :class="cn('flex space-x-3', verticalLayout && 'flex-col space-y-2')">
+            <Label
+                v-if="label"
+                :for="inputId"
+                :class="cn(error && 'text-destructive', labelUppercase && 'uppercase', !verticalLayout && 'flex w-1/4 items-center')"
+            >
+                {{ label }}<RequiredIndicator v-if="isRequired" />
             </Label>
-			<Input
-				v-bind="$attrs"
-				:id="inputId"
-				:class="cn(baseClasses, classes)"
-				:type="type"
-			/>
-		</div>
-		<InputError :class="cn('flex w-full lowercase', !verticalLayout && 'justify-end')" :message="error" />
-	</div>
+
+            <Input v-bind="$attrs" :id="inputId" :class="cn(baseClasses, classes)" :type="type" />
+        </div>
+
+        <InputError :message="error" :class="cn('flex w-full lowercase', !verticalLayout && 'justify-end')" />
+    </div>
 </template>
