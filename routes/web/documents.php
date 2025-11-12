a207ff236bb8d391573c7882f420222b5e788512
penstej <penstejdevelopers@gmail.com>
2025-11-12 11:07:52 +0200

    remove auth on offer letter link

--- routes/web/documents.php
+++ routes/web/documents.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\Documents\DocumentController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('documents')->middleware('auth')->group(function () {
+Route::prefix('documents')->group(function () {
     # ==================================== OFFER LETTER ======================================================
     Route::get('offer-letter/{student_program}', [DocumentController::class, 'previewOfferLetter'])->name('documents.offer-letter');
 });
