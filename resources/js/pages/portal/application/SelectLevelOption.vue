97b9a363973eca38cf0b34b32e59109cf87d4ba8
penstej <penstejdevelopers@gmail.com>
2026-01-15 15:49:10 +0200

    select value

---
+++ resources/js/pages/portal/application/SelectLevelOption.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+// UI components
+import { computed, onMounted, ref } from 'vue';
+
+
+// Utilities
+
+// Props
+interface Props {
+    auth: AuthObject;
+    errors: object;
+}
+
+const props = defineProps<Props>();
+const { user } = props.auth;
+
+onMounted(async () => {
+});
+
+
+const save = async () => {
+
+};
+</script>
+<template>
+    <StudentPageHeader />
+    <form @submit.prevent="() => save()">
+        <div class="mt-20 flex w-full flex-col bg-white px-5 md:p-0">
+
+        </div>
+    </form>
+</template>
