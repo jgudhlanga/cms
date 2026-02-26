bec7ee8c9034656340d699bc582492cc19b1ae99
penstej <penstejdevelopers@gmail.com>
2026-02-26 17:40:49 +0200

    pass applicationDatec

--- app/Console/Commands/Students/BulkProcessOfferLettersCommand.php
+++ app/Console/Commands/Students/BulkProcessOfferLettersCommand.php
@@ -11,13 +11,14 @@ use App\Models\Enrolments\ClassList;
 use App\Models\Institution\DepartmentApplicationStep;
 use App\Models\Shared\WorkflowStep;
 use App\Models\Students\StudentProgram;
+use Carbon\Carbon;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
 use Throwable;
 
 class BulkProcessOfferLettersCommand extends Command
 {
-    protected $signature = 'app:bulk-process-offer-letters-command';
+    protected $signature = 'app:bulk-process-offer-letters-command {applicationDate}';
 
     protected $description = 'Bulk generate offer letters for students';
 
@@ -26,6 +27,8 @@ class BulkProcessOfferLettersCommand extends Command
         $successCount = 0;
         $failedCount = 0;
 
+        $applicationDate = $this->argument('applicationDate');
+
         $acceptedStep = WorkflowStep::where('slug', WorkflowStepEnum::ACCEPTED->slug())->first();
 
         if (!$acceptedStep) {
@@ -33,7 +36,7 @@ class BulkProcessOfferLettersCommand extends Command
             return;
         }
 
-        StudentProgram::with(['departmentWorkflowStep.workflowStep', 'student.user'])
+        $query = StudentProgram::with(['departmentWorkflowStep.workflowStep', 'student.user'])
             ->whereHas('departmentLevel.level', function ($query) {
                 $query->whereIn('name', [LevelEnum::ND, LevelEnum::HND]);
             })
@@ -46,75 +49,76 @@ class BulkProcessOfferLettersCommand extends Command
                     WorkflowStepEnum::REJECTED,
                     WorkflowStepEnum::ACCEPTED,
                 ]);
-            })
-            ->chunkById(100, function ($programs) use ($acceptedStep, &$successCount, &$failedCount) {
-
-                foreach ($programs as $program) {
-                    try {
-                        DB::transaction(function () use ($program, $acceptedStep) {
-
-                            $classList = ClassList::firstOrNew([
-                                'student_program_id' => $program->id
-                            ]);
-
-                            $classList->fill([
-                                'tenant_id' => $program->tenant_id,
-                                'type' => 'verified',
-                                'attributes' => array_merge(
-                                    $classList->attributes ?? [],
-                                    [
-                                        'identity_confirmed' => false,
-                                        'disability_confirmed' => false,
-                                        'names_confirmed' => false,
-                                        'o_level_confirmed' => false,
-                                        'previous_level_confirmed' => false,
-                                    ]
-                                ),
-                            ]);
-
-                            $classList->save();
-
-                            $studentNumber = EnrolmentHelper::resolveStudentNumber($program);
-
-                            $program->student->update([
-                                'student_number' => $studentNumber,
-                                'student_number_generated' => true,
-                            ]);
-
-                            $departmentStep = DepartmentApplicationStep::where(
-                                'institution_department_id',
-                                $program->institution_department_id
-                            )->where(
-                                'workflow_step_id',
-                                $acceptedStep->id
-                            )->first();
-
-                            if (!$departmentStep) {
-                                throw new \Exception('Department step not found.');
-                            }
-
-                            $program->update([
-                                'department_application_step_id' => $departmentStep->id
-                            ]);
-
-                            $user = $program->student->user;
-
-                            SendOfferLetterJob::dispatch(
-                                $user->full_name,
-                                $user->email,
-                                $program->id
-                            )->withoutDelay();
-                        });
-
-                        $successCount++;
-                        $this->info("Offer letter generated successfully. {$program->id}");
-
-                    } catch (Throwable $e) {
-                        $failedCount++;
-                        $this->error("Offer letter failed for {$program->id}: {$e->getMessage()}");
-                    }
-                }
             });
+        $parsedDate = Carbon::parse($applicationDate)->startOfDay();
+        $query->where('student_programs.created_at', '>=', $parsedDate);
+        $query->chunkById(100, function ($programs) use ($acceptedStep, &$successCount, &$failedCount) {
+
+            foreach ($programs as $program) {
+                try {
+                    DB::transaction(function () use ($program, $acceptedStep) {
+
+                        $classList = ClassList::firstOrNew([
+                            'student_program_id' => $program->id
+                        ]);
+                        $classList->fill([
+                            'tenant_id' => $program->tenant_id,
+                            'type' => 'verified',
+                            'attributes' => array_merge(
+                                $classList->attributes ?? [],
+                                [
+                                    'identity_confirmed' => false,
+                                    'disability_confirmed' => false,
+                                    'names_confirmed' => false,
+                                    'o_level_confirmed' => false,
+                                    'previous_level_confirmed' => false,
+                                ]
+                            ),
+                        ]);
+
+                        $classList->save();
+
+                        $studentNumber = EnrolmentHelper::resolveStudentNumber($program);
+
+                        $program->student->update([
+                            'student_number' => $studentNumber,
+                            'student_number_generated' => true,
+                        ]);
+
+                        $departmentStep = DepartmentApplicationStep::where(
+                            'institution_department_id',
+                            $program->institution_department_id
+                        )->where(
+                            'workflow_step_id',
+                            $acceptedStep->id
+                        )->first();
+
+                        if (!$departmentStep) {
+                            throw new \Exception('Department step not found.');
+                        }
+
+                        $program->update([
+                            'department_application_step_id' => $departmentStep->id
+                        ]);
+
+                        $user = $program->student->user;
+
+                        SendOfferLetterJob::dispatch(
+                            $user->full_name,
+                            $user->email,
+                            $program->id
+                        )->withoutDelay();
+                    });
+
+                    $successCount++;
+                    $this->info("Offer letter generated successfully. {$program->id}");
+
+                } catch (Throwable $e) {
+                    $failedCount++;
+                    $this->error("Offer letter failed for {$program->id}: {$e->getMessage()}");
+                }
+            }
+        });
 
         $this->line('');
         $this->info("Bulk processing completed.");
