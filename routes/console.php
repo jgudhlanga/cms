4a18421e69a63f935f7a0cd5e91fbbe9673b477a
penstej <penstejdevelopers@gmail.com>
2026-03-24 14:43:32 +0200

    fixes

--- routes/console.php
+++ routes/console.php
@@ -8,6 +8,4 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command('payments:dispatch')
-    ->everyTenMinutes()
-    ->withoutOverlapping();
+Schedule::command('payments:dispatch')->everyTenMinutes()->withoutOverlapping();
