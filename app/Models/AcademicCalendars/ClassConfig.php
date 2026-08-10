6b4f78e85903248c75e0394c174a0853a81ed8af
James Gudhlanga <jimmyneds@mail.com>
2026-08-10 16:49:26 +0200

    error fix

--- app/Models/AcademicCalendars/ClassConfig.php
+++ app/Models/AcademicCalendars/ClassConfig.php
@@ -6,15 +6,15 @@ use App\Models\Institution\DepartmentCourse;
 use App\Models\Institution\DepartmentLevel;
 use App\Models\Institution\InstitutionDepartment;
 use App\Models\Institution\ModeOfStudy;
+use App\Models\Institution\Syllabus\CourseSyllabus;
+use App\Relations\BelongsToArrayIds;
 use App\Traits\Paginatable;
-use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Spatie\Activitylog\LogOptions;
 use Spatie\Activitylog\Traits\LogsActivity;
-use App\Models\Institution\Syllabus\CourseSyllabus;
 
 /**
  * @mixin Builder
@@ -31,11 +31,13 @@ class ClassConfig extends Model
         'course_syllabus_ids' => 'array',
     ];
 
-     protected $appends = ['syllabus'];
-
-    public function getSyllabusAttribute(): Collection
+    public function syllabus(): BelongsToArrayIds
     {
-        return CourseSyllabus::whereIn('id', $this->course_syllabus_ids ?? [])->get();
+        return new BelongsToArrayIds(
+            CourseSyllabus::query(),
+            $this,
+            'course_syllabus_ids'
+        );
     }
 
     public function institutionDepartment(): BelongsTo
